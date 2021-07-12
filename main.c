32d9668d4677cdc5d3390c757d767056e6574828
Barr <Barrcap1@gmail.com>
2021-07-12 09:39:34 -0400

    starting with main

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 
+#define FILE_NAME_SIZE 40
 
 int main(int argc, char const *argv[])
 {
 	
+	if (argc == 1)
+		printf("No files were recieved for compilation!\n");
+	else
+	{
+		
+	}
+
+
 	return 0;
 }

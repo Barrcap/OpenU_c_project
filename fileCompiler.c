cfd2dbaeb525dc2f54b85ddf2a3d12fbdc707be2
Barr <Barrcap1@gmail.com>
2021-07-23 18:43:10 +0300

    started working on encodingLine function

--- fileCompiler.c
+++ fileCompiler.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 #include "data.h"
 #include "fileCompiler.h"
 
@@ -34,7 +36,7 @@ int fileCompiler(char *fileName)
 	while (!reachedEOF)
 	{
 		if (readFileLine(file, line, &reachedEOF, &codingData) == 0)
-			errorCounter += encodingLineTake1(line);
+			errorCounter += encodingLineTake1(line, &codingData);
 		else
 		{
 			printError("Line too long", &codingData);
@@ -52,7 +54,7 @@ int fileCompiler(char *fileName)
 	while (!reachedEOF)
 	{
 		if (readFileLine(file, line, &reachedEOF, &codingData) == 0)
-			errorCounter += encodingLineTake2(line);
+			errorCounter += encodingLineTake2(line, &codingData);
 		codingData.sourceLine ++;
 	}
 	
@@ -85,12 +87,23 @@ int readFileLine(FILE *file, char *line, int *reachedEOF, fileCodingStruct *codi
 	return 0;
 }
 
-int encodingLineTake1(char *line)
+int encodingLineTake1(char *line, struct fileCodingStruct *codingData)
 {
+	char lable[LABLE_SIZE];
+	char command[COMMAND_SIZE];
+	char operands[OPERANDS_SIZE];
+	/* Todo - add lable and command lengh validation */
+
+	/*
+	sscanf(line, lable);
+	if (lable[strlen(lable)-1] == ':');*/
+
+
+
 	return 0;
 }
 
-int encodingLineTake2(char *line)
+int encodingLineTake2(char *line, struct fileCodingStruct *codingData)
 {
 	return 0;
 }
@@ -101,6 +114,51 @@ void printError(char *errorString, struct fileCodingStruct *codingData)
 	printf(RED "%s:%i: " RESET, codingData->fileName, codingData->sourceLine);
 	printf("%s\n", errorString);
 }
+
+
+int seperateArguments(char *line, char *lable, char command, char *operands, struct fileCodingStruct *codingData)
+{
+	char c;
+	int i, p;
+
+	while (isspace(line[i]))
+		/* skips white notes at the beginning */
+		i++;
+
+	p=i; /* p is now operand first character */
+
+	while (!isspace(line[i]))
+		i++;
+
+
+	/*char firstOperand[LINE_LENGTH];
+	char secondOperand[LINE_LENGTH];*/
+
+	/*sscanf(line, "%s %s", firstOperand, secondOperand);
+
+	if (firstOperand[strlen(firstOperand)-1] == ':')
+	{
+		/ * First string until white note is a lable * /
+		if (strlen(firstOperand) <= LABLE_SIZE)
+			strcpy(lable, firstOperand);
+		else
+		{
+			printError("Lable is too long", codingData);
+			return 1;
+		}
+
+		if (strlen(secondOperand) <= COMMAND_SIZE)
+			strcpy(command, secondOperand);
+		else
+		{
+			printError("Invalid command name", codingData);
+			return 1;
+		}
+	}*/
+
+	return 0;
+}
+
 
 /*##################################################################################*/
 long int roiEncoding(char *command, char *operands) /*dummy function Roi's encoding function */
--- fileCompiler.h
+++ fileCompiler.h
@@ -8,8 +8,9 @@ struct fileCodingStruct;
 
 int fileCompiler(char *fileName);
 int readFileLine(FILE *file, char *line, int *reachedEOF, struct fileCodingStruct *codingData);
-int encodingLineTake1(char *line);
-int encodingLineTake2(char *line);
+int encodingLineTake1(char *line, struct fileCodingStruct *codingData);
+int encodingLineTake2(char *line, struct fileCodingStruct *codingData);
+int seperateArguments(char *line, char *lable, char command, char *operands, struct fileCodingStruct *codingData);
 
 /*##################################################################################*/
 long int roiEncoding(char *command, char *operands); /*dummy function Roi's encoding function */

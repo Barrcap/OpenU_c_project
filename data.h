14df8e10a7690492703a1bea55ff99260266730e
Barr <Barrcap1@gmail.com>
2021-07-26 16:21:21 +0300

    started working on table pushing func

--- data.h
+++ data.h
@@ -63,6 +63,7 @@ typedef struct fileCodingStruct
 
 	int ic; /* instruction commands counter */
 	int dc; /* data commands counter */
+	int sc; /* symbol table counter */
 	int icf; /* amount of instruction commands*/
 	int dcf; /* amount of data commands */
 	int sourceLine;
@@ -76,6 +77,8 @@ typedef struct fileCodingStruct
 int createTables(fileCodingStruct *codingData);
 int expandTable(int tableType, fileCodingStruct *codingData);
 void freeTables(fileCodingStruct *codingData);
+
+void addToTable(fileCodingStruct *codingData, int tableType, ...);
 
 void resetCounterParams(fileCodingStruct *codingData);
 

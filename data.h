2447599d9988a9212931dedeeb6e0640d482e97a
David Sariel <davidsa@openu.ac.il>
2021-07-26 15:23:06 +0300

    test update

--- data.h
+++ data.h
@@ -34,7 +34,7 @@ typedef struct fileCodingStruct
 /*	
 	General structure that contains all 3 data tables and relevant valiables for encoding.
 	will be crated when starting to work on a file, made in order to easily give relevant
-	fucntions easy access to the data.
+	fucntions easy access to the data. 
 */
 
 	symbolLink *symbolLinkHead;

d8ba3fb26b80f507782904755a573eff3e642104
Barr <Barrcap1@gmail.com>
2021-07-27 22:18:58 +0300

    started working on pushCode

--- data.c
+++ data.c
@@ -11,17 +11,19 @@ int createTables(fileCodingStruct *codingData)
 
 	codingData->iTable = (dataCell*) calloc(TABLE_BUFFER, sizeof(dataCell));
 	codingData->iTableSize = TABLE_BUFFER;
+	codingData->iCurrIndex = 0;
 	if (codingData->iTable == NULL)
 		return 1;
 
 	codingData->dTable = (dataCell*) calloc(TABLE_BUFFER, sizeof(dataCell));
 	codingData->dTableSize = TABLE_BUFFER;
+	codingData->dCurrIndex = 0;
 	if (codingData->dTable == NULL)
 		return 1;
 
 	codingData->sTable = (symbolCell*) calloc(TABLE_BUFFER, sizeof(symbolCell));
 	codingData->sTableSize = TABLE_BUFFER;
-	codingData->sc = 0;
+	codingData->sCurrIndex = 0;
 	if (codingData->sTable == NULL)
 		return 1;
 
@@ -38,32 +40,44 @@ void freeTables(fileCodingStruct *codingData)
 	
 }
 
-int expandTable(int tableType, fileCodingStruct *codingData)
-{	/* function gets table type with general struct and expand relevant table.
+int expandTableIfNeeded(int tableType, fileCodingStruct *codingData)
+{	/* function gets table type with general struct and expand relevant table if needed.
 	return 0 is succeeded, 1 if failed. */
 	switch (tableType)
 	{
 		case I_TABLE:
+
+			if (codingData->iCurrIndex < codingData->iTableSize)
+				return 0; /* no need to expand table */
+
 			codingData->iTableSize += TABLE_BUFFER;
 			codingData->iTable = (dataCell*) realloc(codingData->iTable, codingData->iTableSize);
 			if (codingData->iTable == NULL)
-				return 1;
+				return 1; /* failed to allocate more memory */
 
 			break;
 
 		case D_TABLE:
+
+			if (codingData->dCurrIndex < codingData->dTableSize)
+				return 0; /* no need to expand table */
+
 			codingData->dTableSize += TABLE_BUFFER;
 			codingData->dTable = (dataCell*) realloc(codingData->dTable, codingData->dTableSize);
 			if (codingData->dTable == NULL)
-				return 1;
+				return 1; /* failed to allocate more memory */
 
 			break;
 
 		case S_TABLE:
+
+			if (codingData->sCurrIndex < codingData->sTableSize)
+				return 0; /* no need to expand table */
+
 			codingData->sTableSize += TABLE_BUFFER;
 			codingData->sTable = (symbolCell*) realloc(codingData->sTable, codingData->sTableSize);
 			if (codingData->sTable == NULL)
-				return 1;
+				return 1; /* failed to allocate more memory */
 
 			break;
 	}
@@ -71,26 +85,6 @@ int expandTable(int tableType, fileCodingStruct *codingData)
 	return 0;
 }
 
-/*void addToTable(fileCodingStruct *codingData, int tableType, ...)
-{
-	va_list args;
-
-
-	switch (tableType)
-	{
-		case I_TABLE:  machineCode wasCoded 
-			
-			break;
-
-		case D_TABLE:  machineCode, byteAmount 
-
-			break;
-
-		case S_TABLE:  *name, adress, placing, visibility 
-
-			break;
-	}
-}*/
 
 void resetCounterParams(fileCodingStruct *codingData)
 {
@@ -109,22 +103,31 @@ int getIC(fileCodingStruct *codingData)
 	return 0;
 }
 
-void pushCode(long int code, fileCodingStruct *codingData)
+int pushCode(long int code, fileCodingStruct *codingData)
 {
-	/* Temprorary until start using tables: ############################### */
-	codingData->code = code;
+	if (expandTableIfNeeded(I_TABLE, codingData))
+		return -1; /* failed expanding table */
+
+	codingData->iTable[codingData->iCurrIndex].adress = codingData->ic;
+	codingData->iCurrIndex ++;
+	codingData->ic += 4;
+
+
+	return 0;
+
+	/* Temprorary until start using tables: ############################### * /
+	codingData->code = code;*/
 }
 
 int pushLable(char *label, int placing, fileCodingStruct *codingData)
 {/*	
-	returns 0 on success, 1 if found error, -1 if failed to realloc symbol table size
-*/
+	returns 0 on success, 1 if found error, -1 if failed to realloc symbol table size */
 	int i=0;
 	/*char *errorString[LINE_LENGTH];
 	char *sourceLineString[20];*/
 
 	/* checking if label was already defined: */
-	for (i=0; i<codingData->sc; i++) 
+	for (i=0; i<codingData->sCurrIndex; i++) 
 	{
 		if (strcmp(label,codingData->sTable[i].name) == 0)
 		{
@@ -153,13 +156,10 @@ int pushLable(char *label, int placing, fileCodingStruct *codingData)
 		}
 	}
 	/* looking for label loop ended.
-	i=sc, pointing to next available spot in the table */
+	i=sCurrIndex, pointing to next available spot in the table */
 
-	if (i>=codingData->sTableSize) /* expanding symbol table if needed */
-	{
-		if (expandTable(S_TABLE, codingData))
-			return -1; /* failed expanding tabel */
-	}
+	if (expandTableIfNeeded(S_TABLE, codingData))
+		return -1; /* failed expanding table */
 
 	strcpy(codingData->sTable[i].name, label);
 	if (placing == CODE_IMAGE)
@@ -174,3 +174,13 @@ int pushLable(char *label, int placing, fileCodingStruct *codingData)
 	return 0;
 }
 
+void finalizeSymbolTable(fileCodingStruct *codingData)
+{
+	int i=0;
+
+	for (i=0; i<codingData->sCurrIndex; i++)
+	{
+		if (codingData->sTable[i].placing == DATA_IMAGE)
+			codingData->sTable[i].adress += codingData->icf;
+	}
+}
